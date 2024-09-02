b719c5b842fd5f6516483d2ccd83cbfb412b6a14
jeas-github <jeas@outlook.com>
2024-09-01 21:13:39 -0300

    Alteração do index.php

--- src/conexao.php
+++ src/conexao.php
@@ -1,14 +1,14 @@
 <?php
-    // Arquivo: conexao.php
-    // ==================================================================
+// Arquivo: conexao.php
+// ==================================================================
 
-    // Cria uma conexão com um banco de dados MySQL usando a classe PDO. 
-    // A instância criada pode ser usada posteriormente para executar consultas e 
-    // operações no banco de dados.
-    // 'mysql:host=setup-mysql;dbname=db_name': Esse é o DSN (Data Source Name), 
-    // que especifica o host (no exemplo, setup-mysql) e o 
-    // nome do banco de dados (no exemplo, db_name).
-    // 'user': Nome de usuário usado para autenticação no banco de dados.
-    // 'password': Senha usada para autenticação no banco de dados.
-    return new PDO('mysql:host=setup-mysql;dbname=db_name', 'user', 'password');
+// Cria uma conexão com um banco de dados MySQL usando a classe PDO. 
+// A instância criada pode ser usada posteriormente para executar consultas e 
+// operações no banco de dados.
+// 'mysql:host=setup-mysql;dbname=db_name': Esse é o DSN (Data Source Name), 
+// que especifica o host (no exemplo, setup-mysql) e o 
+// nome do banco de dados (no exemplo, db_name).
+// 'user': Nome de usuário usado para autenticação no banco de dados.
+// 'password': Senha usada para autenticação no banco de dados.
 
+return new PDO('mysql:host=setup-mysql;dbname=db_name', 'user', 'password');
